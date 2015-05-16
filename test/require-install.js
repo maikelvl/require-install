a757ce2b49c6fb4ab4682ebe6d172f457918a1f0
Maikel Vlasman <maikel@userex.nl>
2015-05-16 14:36:56 +0000

    initial commit

---
+++ test/require-install.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var require_install = require('../lib/require-install.js');
+var fs = require('fs');
+
+describe("require-install", function() {
+
+	it("it runs", function() {
+
+	});
+
+});
